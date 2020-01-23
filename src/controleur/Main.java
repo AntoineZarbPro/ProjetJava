35cdb456acc00499af18c52fe46a62a9589a79ac
zarb <zarb@localhost>
2020-01-23 14:30:51 +0100

    Version Finale

--- src/controleur/Main.java
+++ src/controleur/Main.java
@@ -4,7 +4,20 @@ public class Main {
 
 public static void main (String [] args) {
 		
+	for (int x=0;x<11;x++) {
+		System.out.println(" 5 x "+x+ "="+ (5*x));
+	}
 	
+	System.out.println(" ________");
+	System.out.println("|");
+	System.out.println("|");
+	System.out.println("|");
+	System.out.println(" ________");
+	System.out.println("	|");
+	System.out.println("	|");
+	System.out.println("	|");
+	System.out.println("	|");
+	System.out.println(" ________");
 		
 	}
 }
